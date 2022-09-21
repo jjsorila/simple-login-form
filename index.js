3a544cc8d6670d9f28f05ac93c7fffa71952cf69
JJS <jjsorila@gmail.com>
2022-09-21 10:25:09 +0800

    more changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let slideBtn = document.getElementById("slide-btn");
 let slider = document.getElementById("slider")
 let handler = true
+
 slideBtn.addEventListener("click", function (e) {
     slider.classList.toggle("animation-slide");
+
     if(handler){
         this.textContent = "REGISTER"
         handler = !handler
